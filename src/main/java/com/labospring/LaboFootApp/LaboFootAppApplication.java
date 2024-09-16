1f9caf732bf3dfabba78400e4244bddd7e494c86
Stephane Kalonji <kalonjistephane@gmail.com>
2024-09-16 19:21:59 +0200

    Project init

---
+++ src/main/java/com/labospring/LaboFootApp/LaboFootAppApplication.java
@@ -0,0 +1,13 @@
+package com.labospring.LaboFootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboFootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboFootAppApplication.class, args);
+	}
+
+}
