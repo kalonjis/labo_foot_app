36052da594f37f5830d750ea77d5871c2c0489a7
kalonjis <kalonjistephane@gmail.com>
2024-09-18 09:52:09 +0200

    add gitignore

--- src/main/java/com/labospring/LaboFootApp/LaboFootAppApplication.java
+++ src/main/java/com/labospring/LaboFootApp/LaboFootAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LaboFootAppApplication {
 
 	public static void main(String[] args) {
-//githubtest
+
 		SpringApplication.run(LaboFootAppApplication.class, args);
 	}
 
