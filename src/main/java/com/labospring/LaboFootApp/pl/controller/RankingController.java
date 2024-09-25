da2a3cfe11072183c39c2bad3bbcaee57827d303
kalonjis <kalonjistephane@gmail.com>
2024-09-25 10:19:22 +0200

    CRUD for Ranking - endpoint get one + refactor/renaming RankingBusiness -> without "t"

--- src/main/java/com/labospring/LaboFootApp/pl/controller/RankingController.java
+++ src/main/java/com/labospring/LaboFootApp/pl/controller/RankingController.java
@@ -6,6 +6,7 @@ import com.labospring.LaboFootApp.pl.models.ranking.RankingDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,14 @@ public class RankingController {
                 .stream()
                 .map(RankingDTO::fromEntity)
                 .toList()
+        );
+    }
+
+
+    @GetMapping("/{id:^\\d+}")
+    public ResponseEntity<RankingDTO> get(@PathVariable long id){
+        return ResponseEntity.ok(
+                RankingDTO.fromEntity(rankingService.getOne(id))
         );
     }
 }
