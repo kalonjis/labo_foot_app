713d32b33e3df7c7bf6474327ea7ff38839f73da
David VK <david.vankeer@gmail.com>
2024-09-26 16:52:46 +0200

    Update the bracket when a winner is found (via FINISHED status of FootMatch.

--- src/main/java/com/labospring/LaboFootApp/dal/repositories/BracketRepository.java
+++ src/main/java/com/labospring/LaboFootApp/dal/repositories/BracketRepository.java
@@ -1,13 +1,18 @@
 package com.labospring.LaboFootApp.dal.repositories;
 
 import com.labospring.LaboFootApp.dl.entities.Bracket;
+import com.labospring.LaboFootApp.dl.entities.FootMatch;
 import com.labospring.LaboFootApp.dl.entities.Tournament;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BracketRepository extends JpaRepository<Bracket, Long> {
     @Query("select b from Bracket b where b.tournament = :tournament")
     List<Bracket> findAllByTournament(Tournament tournament);
+
+    @Query("SELECT b.positionBracket FROM Bracket b WHERE b.match = :footMatch")
+    Integer findPositionByFootMatch(@Param("footMatch") FootMatch footMatch);
 }
