3f0d76ef6a4886c359f03f8e99f7a2b3cbe80605
Stephane Kalonji <kalonjistephane@gmail.com>
2024-09-26 12:29:32 +0200

    CRUD for ParticipatingTeam - endPoint getOne

--- src/main/java/com/labospring/LaboFootApp/bll/service/ParticipatingTeamService.java
+++ src/main/java/com/labospring/LaboFootApp/bll/service/ParticipatingTeamService.java
@@ -4,6 +4,5 @@ import com.labospring.LaboFootApp.bll.service.models.ParticipatingTeamBusiness;
 import com.labospring.LaboFootApp.dl.entities.ParticipatingTeam;
 
 public interface ParticipatingTeamService extends BaseService<Long, ParticipatingTeam, ParticipatingTeamBusiness> {
-
     ParticipatingTeam getOneById(ParticipatingTeam.ParticipatingTeamId id);
 }
