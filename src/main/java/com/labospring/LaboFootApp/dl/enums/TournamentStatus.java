22c8705765c9cc5f26410cbf6a124a38c8de8d8a
Stephane Kalonji <kalonjistephane@gmail.com>
2024-09-30 16:04:29 +0200

    add method to check if a value is one of the "TournamentStatus" enum: "contains"

--- src/main/java/com/labospring/LaboFootApp/dl/enums/TournamentStatus.java
+++ src/main/java/com/labospring/LaboFootApp/dl/enums/TournamentStatus.java
@@ -1,10 +1,21 @@
 package com.labospring.LaboFootApp.dl.enums;
 
+import java.util.Arrays;
+
 public enum TournamentStatus {
     BUILDING,
     PENDING,
     STARTED,
     INTERRUPTED,
     CLOSED,
-    CANCELED
+    CANCELED;
+
+    public static boolean contains(TournamentStatus status) {
+        for (TournamentStatus s : values()) {
+            if (s == status) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
