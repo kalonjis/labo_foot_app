ecf43200db81266f9181fde24625c42dad5ed5c4
kalonjis <kalonjistephane@gmail.com>
2024-10-18 10:25:34 +0200

    add attribute "enabled" to handle the user activation (+constructors: enabled=false;)

--- src/main/java/com/labospring/LaboFootApp/dl/entities/User.java
+++ src/main/java/com/labospring/LaboFootApp/dl/entities/User.java
@@ -43,26 +43,43 @@ public class User extends BaseEntity implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Setter
+    @Column(nullable = false)
+    private boolean enabled;
+
     public User(String username, String password) {
         this.username = username;
         this.password = password;
-    }
-
-    public User(String username, String password, String firstname, String lastname, String email, LocalDate birthdate, String phoneNumber, Address address) {
-        this(firstname, lastname, birthdate, phoneNumber, address);
-        this.username = username;
-        this.password = password;
-        this.email = email;
+        this.enabled = false;
     }
 
     public User(String firstname, String lastname, LocalDate birthdate, String phoneNumber, Address address) {
         this.firstname = firstname;
         this.lastname = lastname;
-
         this.birthdate = birthdate;
         this.phoneNumber = phoneNumber;
         this.address = address;
+        this.enabled = false;
     }
+    public User(String username, String password, String firstname, String lastname, String email, LocalDate birthdate, String phoneNumber, Address address) {
+        this(firstname, lastname, birthdate, phoneNumber, address);
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.enabled = false;
+
+    }
+
+    public User(String username, String password, String firstname, String lastname, String email, LocalDate birthdate, String phoneNumber, Address address, Role role) {
+        this(firstname, lastname, birthdate, phoneNumber, address);
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.enabled = false;
+
+    }
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -86,6 +103,6 @@ public class User extends BaseEntity implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return this.enabled;
     }
 }
