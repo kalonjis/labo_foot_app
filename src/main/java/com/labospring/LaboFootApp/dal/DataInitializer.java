e2e74c0438840b23ddf4a2806456a707a283d660
kalonjis <kalonjistephane@gmail.com>
2024-10-18 10:16:30 +0200

    user: setEnabled(true)

--- src/main/java/com/labospring/LaboFootApp/dal/DataInitializer.java
+++ src/main/java/com/labospring/LaboFootApp/dal/DataInitializer.java
@@ -43,22 +43,25 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         // region  User
-        User user1 = new User(null, "steby81", passwordEncoder.encode("test123"), "Steph", "Kal", "kalonj1@springfield.com", LocalDate.of(1981, 5, 12), "0498123456", new Address("123 Main St", "Brussels", "1000", "Brussels", "Belgium"), Role.ADMIN);
-        User user2 = new User(null, "gabinho", passwordEncoder.encode("test123"), "Jean", "Gabin", "kalo@springfield.com", LocalDate.of(1990, 3, 25), "0498765432", new Address("456 Elm St", "Antwerp", "2000", "Antwerp", "Belgium"), Role.ADMIN);
-        User user3 = new User(null, "homerSps", passwordEncoder.encode("test123"), "Homer", "Simpson", "kalon@springfield.be", LocalDate.of(1978, 6, 15), "0476987654", new Address("789 Maple St", "Liège", "4000", "Liège", "Belgium"), Role.USER);
-        User user4 = new User(null, "margeSimps", passwordEncoder.encode("test123"), "Marge", "Simpson", "marge.s@springfield.com", LocalDate.of(1975, 10, 1), "0466554433", new Address("123 Springfield Ave", "Springfield", "5000", "Illinois", "USA"), Role.USER);
-        User user5 = new User(null, "bartSimps", passwordEncoder.encode("test123"), "Bart", "Simpson", "bart.s@springfield.com", LocalDate.of(2005, 4, 1), "0456789123", new Address("456 Springfield Ave", "Springfield", "5001", "Illinois", "USA"), Role.USER);
-        User user6 = new User(null, "lisaSimps", passwordEncoder.encode("test123"), "Lisa", "Simpson", "lisa.s@springfield.com", LocalDate.of(2007, 5, 9), "0478996453", new Address("789 Springfield Ave", "Springfield", "5002", "Illinois", "USA"), Role.USER);
-        User user7 = new User(null, "maggySimps", passwordEncoder.encode("test123"), "Maggy", "Simpson", "maggy.s@springfield.com", LocalDate.of(2010, 8, 12), "0487654321", new Address("101 Springfield Ave", "Springfield", "5003", "Illinois", "USA"), Role.USER);
-        User user8 = new User(null, "nedFlanders", passwordEncoder.encode("test123"), "Ned", "Flanders", "ned.f@springfield.com", LocalDate.of(1965, 2, 27), "0496453123", new Address("111 Oak St", "Springfield", "5004", "Illinois", "USA"), Role.USER);
-        User user9 = new User(null, "moeSzyslak", passwordEncoder.encode("test123"), "Moe", "Szyslak", "moe.s@springfield.com", LocalDate.of(1970, 11, 15), "0467123890", new Address("222 Bar St", "Springfield", "5005", "Illinois", "USA"), Role.USER);
-        User user10 = new User(null, "apuNahasapeemapetilon", passwordEncoder.encode("test123"), "Apu", "Nahasapeemapetilon", "apu.n@kwikemart.com", LocalDate.of(1968, 10, 5), "0467001112", new Address("333 Kwik-E-Mart", "Springfield", "5006", "Illinois", "USA"), Role.USER);
+        User user1 = new User("steby81", passwordEncoder.encode("test123"), "Steph", "Kal", "kalonj1@springfield.com", LocalDate.of(1981, 5, 12), "0498123456", new Address("123 Main St", "Brussels", "1000", "Brussels", "Belgium"), Role.ADMIN);
+        User user2 = new User("gabinho", passwordEncoder.encode("test123"), "Jean", "Gabin", "kalo@springfield.com", LocalDate.of(1990, 3, 25), "0498765432", new Address("456 Elm St", "Antwerp", "2000", "Antwerp", "Belgium"), Role.ADMIN);
+        User user3 = new User("homerSps", passwordEncoder.encode("test123"), "Homer", "Simpson", "kalon@springfield.be", LocalDate.of(1978, 6, 15), "0476987654", new Address("789 Maple St", "Liège", "4000", "Liège", "Belgium"), Role.USER);
+        User user4 = new User("margeSimps", passwordEncoder.encode("test123"), "Marge", "Simpson", "marge.s@springfield.com", LocalDate.of(1975, 10, 1), "0466554433", new Address("123 Springfield Ave", "Springfield", "5000", "Illinois", "USA"), Role.USER);
+        User user5 = new User("bartSimps", passwordEncoder.encode("test123"), "Bart", "Simpson", "bart.s@springfield.com", LocalDate.of(2005, 4, 1), "0456789123", new Address("456 Springfield Ave", "Springfield", "5001", "Illinois", "USA"), Role.USER);
+        User user6 = new User("lisaSimps", passwordEncoder.encode("test123"), "Lisa", "Simpson", "lisa.s@springfield.com", LocalDate.of(2007, 5, 9), "0478996453", new Address("789 Springfield Ave", "Springfield", "5002", "Illinois", "USA"), Role.USER);
+        User user7 = new User("maggySimps", passwordEncoder.encode("test123"), "Maggy", "Simpson", "maggy.s@springfield.com", LocalDate.of(2010, 8, 12), "0487654321", new Address("101 Springfield Ave", "Springfield", "5003", "Illinois", "USA"), Role.USER);
+        User user8 = new User("nedFlanders", passwordEncoder.encode("test123"), "Ned", "Flanders", "ned.f@springfield.com", LocalDate.of(1965, 2, 27), "0496453123", new Address("111 Oak St", "Springfield", "5004", "Illinois", "USA"), Role.USER);
+        User user9 = new User("moeSzyslak", passwordEncoder.encode("test123"), "Moe", "Szyslak", "moe.s@springfield.com", LocalDate.of(1970, 11, 15), "0467123890", new Address("222 Bar St", "Springfield", "5005", "Illinois", "USA"), Role.USER);
+        User user10 = new User("apuNahasapeemapetilon", passwordEncoder.encode("test123"), "Apu", "Nahasapeemapetilon", "apu.n@kwikemart.com", LocalDate.of(1968, 10, 5), "0467001112", new Address("333 Kwik-E-Mart", "Springfield", "5006", "Illinois", "USA"), Role.USER);
 
         List<User> users = List.of(
                 user1, user2, user3, user4, user5, user6, user7, user8, user9, user10
         );
 
         if (teamRepository.count() == 0) {
+            for(User u : users){
+                u.setEnabled(true);
+            }
             userRepository.saveAll(users);
         }
         //endregion
