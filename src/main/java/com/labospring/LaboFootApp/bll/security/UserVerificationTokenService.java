f5aadd7e3e959cb3289755f3d7191dc1cfad60d6
kalonjis <kalonjistephane@gmail.com>
2024-10-19 12:37:20 +0200

    new Interface for UserVerificationTokenServiceImpl

--- src/main/java/com/labospring/LaboFootApp/bll/security/UserVerificationTokenService.java
+++ src/main/java/com/labospring/LaboFootApp/bll/security/UserVerificationTokenService.java
@@ -1,41 +1,13 @@
 package com.labospring.LaboFootApp.bll.security;
 
-
-import com.labospring.LaboFootApp.bll.exceptions.DoesntExistsException;
-import com.labospring.LaboFootApp.dal.repositories.UserVerificationTokenRepository;
 import com.labospring.LaboFootApp.dl.entities.User;
 import com.labospring.LaboFootApp.dl.entities.UserVerificationToken;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
+public interface UserVerificationTokenService {
 
-@Service
-@RequiredArgsConstructor
-public class UserVerificationTokenService {
-
-    private final UserVerificationTokenRepository userVerificationTokenRepository;
-
-    public UserVerificationToken getOne(String token){
-        return userVerificationTokenRepository.findByToken(token).orElseThrow(()-> new DoesntExistsException("this token doesn't match with any user"));
-    }
-
-    public UserVerificationToken createVerificationToken(User user){
-        String token = generateToken();
-        UserVerificationToken userVerificationToken = new UserVerificationToken();
-        userVerificationToken.setUser(user);
-        userVerificationToken.setToken(token);
-        userVerificationToken.setExpiryDate(LocalDateTime.now().plusMinutes(10));
-
-        return userVerificationTokenRepository.save(userVerificationToken);
-    }
-
-
-
-
-    private String generateToken(){
-        return UUID.randomUUID().toString();
-    }
-
+    UserVerificationToken getOne(String token);
+    UserVerificationToken createVerificationToken(User user);
+    void deleteByToken(String token);
+    UserVerificationToken generateNewVerificationToken(String token);
 }
+
