2d8d33555c4a5fe3700bb96d1cf88beb4ec71018
kalonjis <kalonjistephane@gmail.com>
2024-10-19 12:29:58 +0200

    no change

--- src/main/java/com/labospring/LaboFootApp/bll/service/models/FootMatchSpecificationDTO.java
+++ src/main/java/com/labospring/LaboFootApp/bll/service/models/FootMatchSpecificationDTO.java
@@ -2,6 +2,12 @@ package com.labospring.LaboFootApp.bll.service.models;
 
 import java.time.LocalDateTime;
 
-public record FootMatchSpecificationDTO(String team1, String team2, LocalDateTime before, LocalDateTime after, String referee, String fieldLocation, String tournament_title) {
-
-}
+public record FootMatchSpecificationDTO(
+        String team1,
+        String team2,
+        LocalDateTime before,
+        LocalDateTime after,
+        String referee,
+        String fieldLocation,
+        String tournament_title
+) {}
