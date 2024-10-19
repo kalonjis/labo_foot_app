e3d1ce05981367323d2842aa1f69394a4d18385d
kalonjis <kalonjistephane@gmail.com>
2024-10-19 12:36:39 +0200

    added "@Query(REQUEST)" annotation over findByToken to prevent SQL injection

--- src/main/java/com/labospring/LaboFootApp/dal/repositories/UserVerificationTokenRepository.java
+++ src/main/java/com/labospring/LaboFootApp/dal/repositories/UserVerificationTokenRepository.java
@@ -2,6 +2,7 @@ package com.labospring.LaboFootApp.dal.repositories;
 
 import com.labospring.LaboFootApp.dl.entities.UserVerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserVerificationTokenRepository extends JpaRepository<UserVerificationToken, Long> {
 
+    @Query("SELECT t from UserVerificationToken t WHERE t.token ilike :token")
     Optional<UserVerificationToken> findByToken(String token);
+
 }
