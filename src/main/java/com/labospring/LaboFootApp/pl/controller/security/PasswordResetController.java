7c2bac2fa5c65d91d3ce656d3617c32e4f03a8cb
kalonjis <kalonjistephane@gmail.com>
2024-10-20 20:08:45 +0200

    new endpoints: "@GetMapping("/request-passwordtoken")" and @PostMapping("/reset-password")

--- src/main/java/com/labospring/LaboFootApp/pl/controller/security/PasswordResetController.java
+++ src/main/java/com/labospring/LaboFootApp/pl/controller/security/PasswordResetController.java
@@ -5,13 +5,14 @@ import com.labospring.LaboFootApp.bll.service.MailerService;
 import com.labospring.LaboFootApp.bll.service.UserService;
 import com.labospring.LaboFootApp.dl.entities.PasswordResetToken;
 import com.labospring.LaboFootApp.dl.entities.User;
+import com.labospring.LaboFootApp.pl.models.user.PasswordResetForm;
 import com.labospring.LaboFootApp.pl.models.user.UserSearchForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -22,9 +23,45 @@ public class PasswordResetController {
     public final PasswordResetTokenServiceImpl passwordResetTokenService;
     public final MailerService mailerService;
 
-    @GetMapping("/reset-password")
-    public ResponseEntity<String> resetPassword(@RequestParam String token){
-        return ResponseEntity.ok("Hello");
+    @PostMapping("/reset-password")
+    public ResponseEntity<String> resetPassword(@RequestParam String token, @Valid @RequestBody PasswordResetForm form){
+        PasswordResetToken passwordToken = passwordResetTokenService.getOne(token);
+
+        if (passwordToken == null){
+            return ResponseEntity.badRequest().body("Invalid token");
+        }
+
+        // Vérifier si le token a expiré
+        if (passwordToken.getExpiryDate().isBefore(LocalDateTime.now())) {
+            String requestNewTokenUrl = "http://localhost:8080/request-passwordtoken?token=" + token;
+            String message = "Link has expired. Please request a new one at the following link: " +
+                    "<br/> <a href=\"" + requestNewTokenUrl + "\">Request reset password email</a>";
+            return ResponseEntity.badRequest()
+                    .header("Content-Type", "text/html")
+                    .body(message);
+        }
+        userService.resetPassword(passwordToken.getUser().getId(), form.toBusiness());
+
+        return ResponseEntity.ok().body("Thank you.\n" +
+                "your password has been successfully modified. You can now use it to connect to your favorite app. “login") ;
+
+
+
+    }
+
+    @GetMapping("/request-passwordtoken")
+    public ResponseEntity<String> requestNewToken(@RequestParam String token){
+        PasswordResetToken passwordToken = passwordResetTokenService.getOne(token);
+
+        // Si le token est invalide
+        if (passwordToken == null) {
+            return ResponseEntity.badRequest().body("Invalid token");
+        }
+
+        String newToken = passwordResetTokenService.generateNewToken(token, PasswordResetToken.class, 60L).getToken();
+        mailerService.sendPasswordResetEmail(newToken);
+
+        return ResponseEntity.ok("A new confirmation email has been sent.");
     }
 
     @PostMapping("/request-password")
@@ -47,4 +84,5 @@ public class PasswordResetController {
         return ResponseEntity.ok("Check your inbox\n" +
                 "If your e-mail address matches our database, you will receive an e-mail asking you to reset your password.");
     }
+
 }
