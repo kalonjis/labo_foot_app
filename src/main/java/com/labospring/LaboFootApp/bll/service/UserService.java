5e2a096fa4f971f9b77932ccda2d132b13efdb1d
kalonjis <kalonjistephane@gmail.com>
2024-10-20 20:03:58 +0200

    added new method "resetPassword"

--- src/main/java/com/labospring/LaboFootApp/bll/service/UserService.java
+++ src/main/java/com/labospring/LaboFootApp/bll/service/UserService.java
@@ -1,5 +1,6 @@
 package com.labospring.LaboFootApp.bll.service;
 
+import com.labospring.LaboFootApp.bll.service.models.user.PasswordResetFormBusiness;
 import com.labospring.LaboFootApp.bll.service.models.user.UserEditBusiness;
 import com.labospring.LaboFootApp.bll.service.models.user.UserPasswordEditBusiness;
 import com.labospring.LaboFootApp.dl.entities.User;
@@ -9,6 +10,7 @@ import java.util.List;
 public interface UserService extends BaseService<Long, User, User>{
     void updateOne(Long id, UserEditBusiness entityBusiness);
     void updatePassword(Long id, UserPasswordEditBusiness passwordEditBusiness);
+    void resetPassword(Long id, PasswordResetFormBusiness passwordResetFormBusiness);
     List<User> findAllByUsername(String username);
     List<User> getByCriteria(User user);
     List<User> getUsersForMatchAndTeamsWithNotifications(Long matchId, List<Long> teamIds);
