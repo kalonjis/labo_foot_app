cb62180ed4277e560188f750ab27a5c4a34353da
kalonjis <kalonjistephane@gmail.com>
2024-10-21 20:59:10 +0200

    added new "sendEmailVerification" method

--- src/main/java/com/labospring/LaboFootApp/bll/service/MailerService.java
+++ src/main/java/com/labospring/LaboFootApp/bll/service/MailerService.java
@@ -4,6 +4,7 @@ import com.labospring.LaboFootApp.dl.entities.User;
 
 public interface MailerService {
     void sendWelcomeEmail(User user);
+    void sendEmailVerification(User user);
     void sendPasswordResetEmail(String token);
     void sendTournmentNotification(String userEmail, String TournamentTitle, String message);
     void sendNewConfirmation(String token);
