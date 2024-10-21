456ca7415002f6d4d946dc50c0ab072cdbe77067
kalonjis <kalonjistephane@gmail.com>
2024-10-21 20:08:23 +0200

    prevent login for account not activated yet

--- src/main/java/com/labospring/LaboFootApp/bll/security/impl/AuthServiceImpl.java
+++ src/main/java/com/labospring/LaboFootApp/bll/security/impl/AuthServiceImpl.java
@@ -1,10 +1,14 @@
 package com.labospring.LaboFootApp.bll.security.impl;
 
 import com.labospring.LaboFootApp.bll.events.UserRegisteredEvent;
+import com.labospring.LaboFootApp.bll.exceptions.BadEnabledStatusException;
 import com.labospring.LaboFootApp.bll.exceptions.DoesntExistsException;
+import com.labospring.LaboFootApp.bll.exceptions.UserCredentialAlreadyTakenException;
+import com.labospring.LaboFootApp.bll.exceptions.WrongPasswordException;
 import com.labospring.LaboFootApp.bll.security.AuthService;
 import com.labospring.LaboFootApp.dal.repositories.UserRepository;
 import com.labospring.LaboFootApp.dl.entities.User;
+import com.labospring.LaboFootApp.dl.entities.UserVerificationToken;
 import com.labospring.LaboFootApp.dl.enums.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
@@ -20,12 +24,17 @@ public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final ApplicationEventPublisher eventPublisher;
+    private final UserVerificationTokenServiceImpl userVerificationTokenService;
 
     @Override
     public User register(User user) {
         if(userRepository.existsByUsername(user.getUsername())){
-            throw new RuntimeException("User with username " + user.getUsername() + " already exists");
+            throw new UserCredentialAlreadyTakenException("User with username " + user.getUsername() + " already exists");
         }
+        if(userRepository.existsByUserEmail( user.getEmail() ) ){
+            throw new UserCredentialAlreadyTakenException("User with email address " + user.getEmail() + " already exists");
+        }
+
         user.setRole(Role.USER);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         User savedUser =  userRepository.save(user);
@@ -40,7 +49,14 @@ public class AuthServiceImpl implements AuthService {
         User existingUser = (User) loadUserByUsername(username);
         SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if(!passwordEncoder.matches(password, existingUser.getPassword())) {
-            throw new RuntimeException("Incorrect password");
+            throw new WrongPasswordException("Incorrect password");
+        }
+
+        if(!existingUser.isEnabled()){
+            String token = userVerificationTokenService.createToken(existingUser, UserVerificationToken.class, 60L).getToken();
+            String confirmationUrl = "http://localhost:8080/request-confirmtoken?token=" + token;
+            String message = "Account not activated. Please check your email for the confirmation link or request a new one: " + confirmationUrl;
+            throw new BadEnabledStatusException(message);
         }
 
         return existingUser;
@@ -51,6 +67,6 @@ public class AuthServiceImpl implements AuthService {
 
         return userRepository.findByUsername(username)
                 .orElseThrow(
-                () -> new DoesntExistsException("This user account is inactive or doesn't exists"));
+                () -> new DoesntExistsException("This user account doesn't exists"));
     }
 }
