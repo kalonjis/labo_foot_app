738516bbf6088826432dc2a9fc86458b86a2e563
Stephane Kalonji <kalonjistephane@gmail.com>
2024-10-31 13:38:39 +0100

    search user with email only

--- src/main/java/com/labospring/LaboFootApp/pl/models/user/UserSearchForm.java
+++ src/main/java/com/labospring/LaboFootApp/pl/models/user/UserSearchForm.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UserSearchForm(
-        @NotBlank @NotNull
-        String username,
+//        @NotBlank @NotNull
+//        String username,
         @Email
         String email
         ) { }
