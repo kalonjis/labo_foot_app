d3419271c92f2116e71da44b734cd43091bf8e96
Stephane Kalonji <kalonjistephane@gmail.com>
2024-11-05 10:24:07 +0100

    Added global constants: FRONT_URL = "http://localhost:4200" and BACK_URL = "http://localhost:8080"

--- src/main/java/com/labospring/LaboFootApp/il/props/LaboFootProps.java
+++ src/main/java/com/labospring/LaboFootApp/il/props/LaboFootProps.java
@@ -4,4 +4,7 @@ public class LaboFootProps {
     public static final String PASSWORD_REGEX = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{7,}$";
     //From https://ihateregex.io/expr/phone/
     public static final String PHONE_REGEX = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$";
+
+    public static final String FRONT_URL = "http://localhost:4200";
+    public static final String BACK_URL = "http://localhost:8080";
 }
